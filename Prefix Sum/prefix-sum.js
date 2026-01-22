3ba50542110bbc91c860a5b3382bb2578990d71b
boompow <gatewaymesfin@gmail.com>
2026-01-22 23:08:54 +0300

    prefix sum first implementation

--- Prefix Sum/prefix-sum.js
+++ Prefix Sum/prefix-sum.js
@@ -3,23 +3,18 @@ class PrefixSum{
         this.start = start;
         this.end = end;
         this.array = array;
-        this.prefixSumArray = [];
+        this.prefixSumArray = new Array(this.array.length + 1).fill(0);
+
+        // preprocess
+        for(let i=0; i < this.array.length ; i++){
+            const item = this.array[i] + this.prefixSumArray[i]
+            this.prefixSumArray[i+1] = item
+        }
     }
 
     sumRange(){
-        // preprocess
-        for(let i=0; i < this.array.length ; i++){
-            if(i === 0){
-                const item = this.array[i]
-                this.prefixSumArray.push(item)
-            }else{
-                const item = this.array[i] + this.prefixSumArray[i-1]
-                this.prefixSumArray.push(item)
-            }
-        }
-
         // find sum range
-        const sum = this.start === 0 ? this.prefixSumArray[this.end] :this.prefixSumArray[this.end] - this.prefixSumArray[this.start -1]
+        const sum = this.prefixSumArray[this.end +1 ] - this.prefixSumArray[this.start]
         return sum; 
     }
 
@@ -27,6 +22,6 @@ class PrefixSum{
 
 const array = [1,3,5,5,6,7,8,9,0,56,7,53]
 
-const sum = new PrefixSum(array, 0, 6);
+const sum = new PrefixSum(array, 0, 2);
 
 console.log(sum.sumRange())
