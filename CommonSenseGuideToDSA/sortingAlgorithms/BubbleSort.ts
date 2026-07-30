c8dd9bc17b930aebbbd84d029d95c5ed6a3d883b
boompow <gatewaymesfin@gmail.com>
2026-07-30 09:51:21 +0300

    bubble sort: attempt 1

--- CommonSenseGuideToDSA/sortingAlgorithms/BubbleSort.ts
+++ CommonSenseGuideToDSA/sortingAlgorithms/BubbleSort.ts
@@ -3,5 +3,23 @@ export { }
 const arr: number[] = [3, 1, 4, 5, 2, 8, 12, 9, 6, 17, 15]
 
 function bubbleSort(list: number[]) {
-    // we need to monitor if swap was done in each pass through
+    let unsortedUntilIndex = list.length - 1
+    let steps = 0
+
+    while (unsortedUntilIndex > 0) {
+        for (let i = 0; i < unsortedUntilIndex; i++) {
+            if (list[i] > list[i + 1]) {
+                // use JS array destructuring to swap values
+                [list[i], list[i + 1]] = [list[i + 1], list[i]]
+            }
+            steps++
+        }
+        unsortedUntilIndex--
+
+    }
+
+    console.log(steps)
+    return list
 }
+
+console.log(bubbleSort(arr))
