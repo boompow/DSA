36c8fcb516375c1bf67550b0ff39c786331adc1a
boompow <gatewaymesfin@gmail.com>
2026-07-31 11:22:34 +0300

    sortedArrayMerger: attempt 1

---
+++ CommonSenseGuideToDSA/Exercises/sortedArrayMerger/sortedArrayMerger.ts
@@ -0,0 +1,40 @@
+export { }
+
+const arr1: number[] = [1, 4, 10, 20, 35]
+const arr2: number[] = [1, 3, 6, 10, 15, 21, 28]
+
+function sortedArrayMerger(array1: number[], array2: number[]) {
+    let mergedArray = []
+    let pointer1 = 0
+    let pointer2 = 0
+
+    while (pointer1 < array1.length || pointer2 < array2.length) {
+
+        // comparison
+        if (array1[pointer1] < array2[pointer2]) {
+            mergedArray.push(array1[pointer1])
+            pointer1++
+        }
+        else if (array2[pointer2] < array1[pointer1]) {
+            mergedArray.push(array2[pointer2])
+            pointer2++
+        }
+        else if (!array1[pointer1]) {
+            mergedArray.push(array2[pointer2])
+            pointer2++
+        }
+        else if (!array2[pointer2]) {
+            mergedArray.push(array1[pointer1])
+            pointer1++
+        }
+        else {
+            mergedArray.push(array1[pointer1], array2[pointer2])
+            pointer1++
+            pointer2++
+        }
+    }
+
+    return mergedArray
+}
+
+console.log(sortedArrayMerger(arr1, arr2))
