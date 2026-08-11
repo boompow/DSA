6611465137ab7f1844e7b5e34ce0060e52549231
boompow <gatewaymesfin@gmail.com>
2026-08-11 09:02:03 +0300

    staircase problem: attempt 2

--- CommonSenseGuideToDSA/Exercises/recursion/staircaseProblem.ts
+++ CommonSenseGuideToDSA/Exercises/recursion/staircaseProblem.ts
@@ -8,4 +8,10 @@ function staircasePaths(steps: number): number {
     return staircasePaths(steps - 1) + staircasePaths(steps - 2) + staircasePaths(steps - 3);
 }
 
-console.log(staircasePaths(4))
+function staircasePathsAttempt2(steps: number): number {
+    if (steps < 0) return 0;
+    else if (steps === 1 || steps === 0) return 1;
+    return staircasePathsAttempt2(steps - 1) + staircasePathsAttempt2(steps - 2) + staircasePathsAttempt2(steps - 3);
+}
+
+console.log(staircasePathsAttempt2(11))
