89bdc32ffabc4bde0c5be7358b9adb290fc28010
boompow <gatewaymesfin@gmail.com>
2026-08-12 11:26:35 +0300

    triangular numbers: attempt 2

--- CommonSenseGuideToDSA/Exercises/recursion/triangularNumbers.ts
+++ CommonSenseGuideToDSA/Exercises/recursion/triangularNumbers.ts
@@ -1,10 +1,11 @@
 export { }
 
 
-function triangularNumbers(num: number): number {
-    if (num === 0) return 0
-    let value = num + triangularNumbers(num - 1)
-    return value
+function triangularNumbers(num: number): number[] {
+    if (num === 0) return []
+    let prev = triangularNumbers(num - 1)
+    let currentValue = prev.length > 0 ? num + prev[prev.length - 1] : num
+    return [...prev, currentValue]
 }
 
 console.log(triangularNumbers(5))
