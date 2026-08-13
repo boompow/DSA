ac1e79b45a643b0e352d8040440db771f77c6c70
boompow <gatewaymesfin@gmail.com>
2026-08-13 08:52:34 +0300

    find x: attempt 2

--- CommonSenseGuideToDSA/Exercises/recursion/findX.ts
+++ CommonSenseGuideToDSA/Exercises/recursion/findX.ts
@@ -1,17 +1,15 @@
 export { }
 
-function findX(str: string, index = 0): number | string {
+function findX(str: string, index = 0): number {
     if (str.length === 0) {
-        return "not found"
+        return -1
     }
 
-    let currentValue = str[0]
-    console.log(currentValue)
-    if (currentValue.toLowerCase() === "x") {
+    if (str[0].toLowerCase() === "x") {
         return index
     }
 
-    return findX(str.slice(1), ++index)
+    return findX(str.slice(1), index + 1)
 }
 
-console.log(findX("uyfgix"))
+console.log(findX("uyix"))
