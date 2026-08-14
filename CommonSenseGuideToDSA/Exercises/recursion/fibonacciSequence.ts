9bc2966ea5286d091cf2cd43364b3eb57daf2dd3
boompow <gatewaymesfin@gmail.com>
2026-08-14 09:59:59 +0300

    fibonacci sequence with memoization

--- CommonSenseGuideToDSA/Exercises/recursion/fibonacciSequence.ts
+++ CommonSenseGuideToDSA/Exercises/recursion/fibonacciSequence.ts
@@ -8,3 +8,25 @@ function fibonacciSequence(n: number): number {
 }
 
 console.log(fibonacciSequence(7))
+
+
+// recursive solution with memoization using closure and hash table
+function createFibonacci() {
+    const memo: Map<number, number> = new Map()
+
+    return function fibonacciSequence(n: number): number {
+        if (n < 0) throw new Error("Input must be a non-negative integer.");
+        if (n <= 1) return n;
+        if (memo.has(n)) return memo.get(n)!;
+
+        let result = fibonacciSequence(n - 1) + fibonacciSequence(n - 2)
+        memo.set(n, result)
+
+        return result
+    }
+}
+
+let memoizedFibonacciSequence = createFibonacci()
+
+
+console.log(memoizedFibonacciSequence(30))
