4bb428745588a76fee84befee3b62de4816f8d3a
boompow <gatewaymesfin@gmail.com>
2026-09-02 11:24:36 +0300

    binary search tree insertion: attempt 1

--- CommonSenseGuideToDSA/Exercises/binarySearchTree/binarySearchTree.ts
+++ CommonSenseGuideToDSA/Exercises/binarySearchTree/binarySearchTree.ts
@@ -14,26 +14,54 @@ class BinaryNode<T> {
 class BinarySearchTree<T> {
     root: BinaryNode<T> | null = null
 
-    insert(data: T) {
+    insert(data: T): boolean {
         let newNode = new BinaryNode(data)
         if (!this.root) {
             this.root = newNode
+            return true
         }
+
+        let currentNode = this.root
+        while (currentNode !== null) {
+            // I'm going to reject duplicates
+            if (data === currentNode.data) return false
+
+
+            if (data < currentNode.data) {
+                if (currentNode.left) {
+                    currentNode = currentNode.left
+                } else {
+                    currentNode.left = newNode
+                    return true
+                }
+            } else if (data > currentNode.data) {
+                if (currentNode.right) {
+                    currentNode = currentNode.right
+                } else {
+                    currentNode.right = newNode
+                    return true
+                }
+            }
+        }
+
+        // although dead code, TS flags the return as undefined if I remove it
+        return false
+
+
     }
 
-    search(data: T): T | null {
-        if (!this.root) return null;
+    search(data: T): BinaryNode<T> | null {
 
         let current = this.root
 
-        while (current) {
+        while (current !== null) {
             if (current.data === data) {
-                return current.data
+                return current
             }
-            else if (current.data < data && current.right) {
-                current = current.right
-            } else if (current.data > data && current.left) {
+            else if (data < current.data) {
                 current = current.left
+            } else {
+                current = current.right
             }
         }
 
@@ -41,3 +69,13 @@ class BinarySearchTree<T> {
 
     }
 }
+
+
+let BST = new BinarySearchTree()
+
+BST.insert(10)
+BST.insert(1)
+BST.insert(12)
+BST.insert(15)
+
+console.log(BST.search(1))
