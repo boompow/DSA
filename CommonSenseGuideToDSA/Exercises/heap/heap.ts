490307137e2358e7216dafec54aff7440deb8b62
boompow <gatewaymesfin@gmail.com>
2026-09-12 23:19:41 +0300

    heap deletion: attempt 1

--- CommonSenseGuideToDSA/Exercises/heap/heap.ts
+++ CommonSenseGuideToDSA/Exercises/heap/heap.ts
@@ -9,18 +9,18 @@ class Heap<T> {
     }
 
     last() {
-        return this.list[this.list.length]
+        return this.list[this.list.length - 1]
     }
 
-    leftChildIndex(index: number) {
+    leftChildIndex(index: number): number {
         return 2 * index + 1
     }
 
-    rightChildIndex(index: number) {
+    rightChildIndex(index: number): number {
         return 2 * index + 2
     }
 
-    parentIndex(index: number) {
+    parentIndex(index: number): number {
         return Math.floor((index - 1) / 2)
     }
 
@@ -35,8 +35,45 @@ class Heap<T> {
         }
     }
 
-    delete() {
-        this.list[0] = this.list.pop()
+
+    hasGreaterChild(index: number): boolean {
+        return this.list[this.leftChildIndex(index)] && this.list[index] < this.list[this.leftChildIndex(index)]
+            ||
+            this.list[this.rightChildIndex(index)] && this.list[index] < this.list[this.rightChildIndex(index)]
+    }
+
+    findLargerIndex(index: number): number {
+        if (!this.list[this.rightChildIndex(index)]) {
+            return this.leftChildIndex(index)
+        }
+
+        if (this.list[this.rightChildIndex(index)] > this.list[this.leftChildIndex(index)]) {
+            return this.rightChildIndex(index)
+        } else {
+            return this.leftChildIndex(index)
+        }
+    }
+
+    delete(): boolean {
+        if (this.list.length === 0) return false;
+
+        const popped = this.list.pop();
+        if (this.list.length > 0 && popped !== undefined) {
+            this.list[0] = popped;
+        }
+
+        let trickle_down_index: number = 0
+
+        while (this.hasGreaterChild(trickle_down_index)) {
+            let largerChildIndex: number = this.findLargerIndex(trickle_down_index);
+
+            [this.list[largerChildIndex], this.list[trickle_down_index]] =
+                [this.list[trickle_down_index], this.list[largerChildIndex]]
+
+            trickle_down_index = largerChildIndex;
+        }
+
+        return true
 
     }
 
@@ -55,4 +92,4 @@ heap.insert(29)
 heap.insert(52)
 heap.insert(82)
 
-console.log(heap.get())
+console.log(heap.delete())
